d52c4e7362f25fb825ccdbc46c754c613688e292
Sara Regan <regan.sara@gmail.com>
2018-01-23 22:15:21 +0100

    adds module 1 assignment code

---
+++ converter.js
@@ -0,0 +1,22 @@
+const csvconverter = require('csvtojson')
+const csvFilePath = './customer-data.csv'
+const fs = require('fs')
+
+console.log(csvFilePath)
+
+let i = 0
+const destinationFile = fs.createWriteStream('customer-data.json')
+
+destinationFile.write('[')
+
+csvconverter()
+  .fromFile(csvFilePath)
+  .on('json', (jsonObj) => {
+    const prefix = i == 0 ? '' : ',\n'
+    destinationFile.write(prefix + JSON.stringify(jsonObj))
+    i++
+  })
+  .on('end', () => {
+    destinationFile.write(']')
+    destinationFile.end()
+  })
